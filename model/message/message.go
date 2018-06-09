025a3f50f39c17c6913f3ddf662ea896287f2aa8
peddlrph <37572697+peddlrph@users.noreply.github.com>
2018-06-09 14:01:10 +0800

    Added Messages store feature

--- model/message/message.go
+++ model/message/message.go
@@ -3,26 +3,27 @@ package message
 
 import (
 	"database/sql"
-	"encoding/json"
+	//	"encoding/json"
 	"fmt"
 	//"net/http"
 	//"github.com/go-sql-driver/mysql"
-	"github.com/blue-jay/blueprint/model/mobile_ip"
-	"github.com/peddlrph/lib/smsgateway"
-	s "strings"
+	//	"github.com/blue-jay/blueprint/model/mobile_ip"
+	//	"github.com/peddlrph/lib/smsgateway"
+	//	s "strings"
 )
 
 var (
 	// table is the table name.
-	table = "message"
+	table = "messages"
 )
 
 // Item defines the model.
 type Message struct {
-	Address string `json:"address"`
-	Body    string `json:"body"`
-	Msg_Box string `json:"msg_box"`
-	Id      string `json:"_id"`
+	Address sql.NullString `db:"address"`
+	Body    sql.NullString `db:"body"`
+	Msg_Box sql.NullString `db:"msg_box"`
+	Id      int            `db:"id"`
+	Synced  sql.NullString `db:"synced"`
 }
 
 type Item struct {
@@ -53,8 +54,48 @@ func ByID(db Connection, ID string) (Item, bool, error) {
 	return result, err == sql.ErrNoRows, err
 }
 
+func All(db Connection) ([]Message, error) {
+	var messages []Message
+	//c := flight.Context(w, r)
+
+	//Balance_Message := Message{}
+
+	//Balance_Message.Body = "Balance not available"
+
+	//mobile_ip, _, _ := mobile_ip.ByID(db, "1")
+
+	//response, err := smsgateway.GetMessages("http://" + mobile_ip.IP_Address + ":8080/v1/sms/?limit=10000")
+	//if err == nil {
+	//fmt.Println("yeah")
+	//	go smsgateway.WriteMessagesToFile(response)
+	//}
+
+	//mesgs := Item{}
+	//json.Unmarshal([]byte(response), &mesgs)
+
+	err := db.Select(&messages, fmt.Sprintf(`
+		SELECT id, address,msg_box, body,synced
+		FROM %v
+		`, table))
+
+	//messages := mesgs.Messages
+
+	//strList := []string{"Load Wallet", "Commission Fund", "Smart Money"}
+
+	//for i := 0; i < len(messages); i++ {
+	//	if s.Contains(messages[i].Body, strList[0]) && s.Contains(messages[i].Body, strList[1]) && s.Contains(messages[i].Body, strList[2]) {
+	//fmt.Println(messages[i].Body)
+	//		Balance_Message = messages[i]
+	//		break
+	//	}
+	//}
+
+	return messages, err
+}
+
 // All gets all items.
-func All(db Connection) ([]Message, Message, error) {
+/*
+func AllX(db Connection) ([]Message, Message, error) {
 	// var result []Message
 	//c := flight.Context(w, r)
 
@@ -93,7 +134,7 @@ func All(db Connection) ([]Message, Message, error) {
 
 	return messages, Balance_Message, err
 }
-
+*/
 // Create adds an item.
 func Create(db Connection, name string) (sql.Result, error) {
 	result, err := db.Exec(fmt.Sprintf(`
